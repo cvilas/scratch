696c5671bf2808d6b0aea58b51bef28ba0f22869
Vilas Kumar Chitrakaran <vilas.chitrakaran@tra.ai>
2019-06-24 10:15:48 +0100

    minor stuff

--- log_macros/main.cpp
+++ log_macros/main.cpp
@@ -5,6 +5,7 @@
 // - https://github.com/SergiusTheBest/plog
 
 #include <iostream>
+#include <functional>
 
 struct None
 {
@@ -17,47 +18,48 @@ struct LogData
 };
 
 template<typename Begin, typename Value>
-constexpr LogData<std::pair<Begin&&, Value&&>> operator<<(LogData<Begin>&& begin, Value&& value) noexcept
+constexpr LogData<std::pair<Begin&&, Value&&>> operator<< (LogData<Begin>&& begin, Value&& value) noexcept
 {
     return {{ std::forward<Begin>(begin.list), std::forward<Value>(value) }};
 }
 
 template<typename Begin, size_t n>
-constexpr LogData< std::pair<Begin&&, const char*> > operator<<(LogData<Begin>&& begin, const char (&value)[n]) noexcept
+constexpr LogData<std::pair<Begin&&, const char*>> operator<< (LogData<Begin>&& begin, const char (&value)[n]) noexcept
 {
     return {{ std::forward<Begin>(begin.list), value }};
 }
 
-typedef std::ostream& (*PfnManipulator)(std::ostream&);
+using PfnManipulator = std::function<std::ostream&(std::ostream&)>;
+//typedef std::ostream& (*PfnManipulator)(std::ostream&);
 
 template<typename Begin>
-constexpr LogData< std::pair<Begin&&, PfnManipulator> > operator<<(LogData<Begin>&& begin, PfnManipulator value) noexcept
+constexpr LogData<std::pair<Begin&&, PfnManipulator>> operator<< (LogData<Begin>&& begin, PfnManipulator&& value) noexcept
 {
     return {{ std::forward<Begin>(begin.list), value }};
 }
 
 template <typename Begin, typename Last>
-void output(std::ostream& os, std::pair<Begin, Last>&& data)
+void printList(std::ostream& os, std::pair<Begin, Last>&& data)
 {
-    output(os, std::move(data.first));
+    printList(os, std::move(data.first));
     os << data.second;
 }
+
+inline void printList(std::ostream& os, None)
+{ }
 
 template<typename List>
 void Log(const char* file, int line, LogData<List>&& data)
 {
     std::cout << file << ":" << line << ": ";
-    output(std::cout, std::move(data.list));
+    printList(std::cout, std::move(data.list));
     std::cout << std::endl;
 }
-
-inline void output(std::ostream& os, None)
-{ }
 
 #define LOG(msg) (Log(__FILE__, __LINE__, LogData<None>() << msg))
 
 int main()
 {
-  LOG("Hello World!");
+  LOG("Hello" << " World!");
   return 0;
 }
