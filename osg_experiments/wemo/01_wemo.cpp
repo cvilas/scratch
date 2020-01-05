f5f5a5d94b59ada1beb02776a1b2911b8590c580
Vilas Chitrakaran <cvilas@gmail.com>
2020-01-05 17:57:47 +0000

    kinda works

--- osg_experiments/wemo/01_wemo.cpp
+++ osg_experiments/wemo/01_wemo.cpp
@@ -2,6 +2,7 @@
 #include <osg/MatrixTransform>
 #include <osg/PositionAttitudeTransform>
 #include <osgViewer/Viewer>
+#include <osgGA/TrackballManipulator>
 
 #include <hive/robohive.h>
 #include <hive/types/navigation/odometry.h>
@@ -9,6 +10,7 @@
 #include <hive/dds/reader.h>
 #include <hive/logger/dds_log_appender.h>
 #include <hive/mobility/platform_odometry_topic.h>
+#include <hive/wemo/wemo_motion_model.h>
 
 //=====================================================================================================================
 // Demo class to set up visualisation
@@ -32,9 +34,14 @@ private:
   std::exception_ptr exception_ptr_{};
   osg::ref_ptr<osg::Group> osg_root_{nullptr};
   osg::ref_ptr<osg::PositionAttitudeTransform> robot_tr_body_{nullptr};
+  osg::ref_ptr<osg::PositionAttitudeTransform> tr_wheel1_{nullptr};
+  osg::ref_ptr<osg::PositionAttitudeTransform> tr_wheel2_{nullptr};
+  osg::ref_ptr<osg::PositionAttitudeTransform> tr_wheel3_{nullptr};
+  osg::ref_ptr<osg::PositionAttitudeTransform> tr_wheel4_{nullptr};
   std::unique_ptr<hive::dds::Participant> participant_{ nullptr };
   std::unique_ptr<hive::logger::DDSLogAppender> log_appender_{ nullptr };
   std::unique_ptr<OdometryReader> odo_sub_{ nullptr };
+  hive::mobility::WemoMotionModel motion_model_;
 };
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -52,34 +59,44 @@ void Visualiser::initScene()
   osg::ref_ptr<osg::Node> wheel = osgDB::readNodeFile("/home/vilas/projects/mine/scratch/osg_experiments/wemo/model/wemo_wheel.osgt" );
   osg::ref_ptr<osg::Node> body = osgDB::readNodeFile("/home/vilas/projects/mine/scratch/osg_experiments/wemo/model/wemo_body.osgt" );
 
-  /// PositionAttitudeTransform is easier to use than MatrixTransform
+  // rotate to align body frame with our body frame convention (X:forward, Y:port, Z:up)
+  osg::ref_ptr<osg::MatrixTransform> rotate_body = new osg::MatrixTransform(osg::Matrix::rotate(-M_PI/2, 1, 0, 0));
+  rotate_body->addChild(body.get());
+
+  /// \todo:
+  /// - wheel positions should be read from WemoMotionModel
+  /// - we need two different wheel models for left and right wheels
+  /// - set wheel position and orientation properly
+  /// - set transform to rotate wheel about axis
+
+  tr_wheel1_ = new osg::PositionAttitudeTransform;
+  tr_wheel1_->setPosition(osg::Vec3d(0.350, 0.0, 0.350));
+  tr_wheel1_->addChild( wheel.get() );
+
+  tr_wheel2_ = new osg::PositionAttitudeTransform;
+  tr_wheel2_->setPosition(osg::Vec3d(-0.350, 0.0, 0.350) );
+  tr_wheel2_->addChild( wheel.get() );
+
+  tr_wheel3_ = new osg::PositionAttitudeTransform;
+  tr_wheel3_->setPosition(osg::Vec3d(-0.350, 0.0, -0.350) );
+  tr_wheel3_->addChild( wheel.get() );
+
+  tr_wheel4_ = new osg::PositionAttitudeTransform;
+  tr_wheel4_->setPosition(osg::Vec3d(0.350, 0.0, -0.350) );
+  tr_wheel4_->addChild( wheel.get() );
+
+  rotate_body->addChild( tr_wheel1_.get() );
+  rotate_body->addChild( tr_wheel2_.get() );
+  rotate_body->addChild( tr_wheel3_.get() );
+  rotate_body->addChild( tr_wheel4_.get() );
+
   robot_tr_body_ = new osg::PositionAttitudeTransform;
   robot_tr_body_->setPosition( osg::Vec3d(0.0, 0.0, 0.0) );
-  robot_tr_body_->addChild( body.get() );
-
-  osg::ref_ptr<osg::MatrixTransform> tr_wheel1 = new osg::MatrixTransform;
-  tr_wheel1->setMatrix( osg::Matrix::translate(.350, .0, 0.350) );
-  tr_wheel1->addChild( wheel.get() );
-
-  osg::ref_ptr<osg::MatrixTransform> tr_wheel2 = new osg::MatrixTransform;
-  tr_wheel2->setMatrix( osg::Matrix::translate(-.350, .0, 0.350) );
-  tr_wheel2->addChild( wheel.get() );
-
-  osg::ref_ptr<osg::MatrixTransform> tr_wheel3 = new osg::MatrixTransform;
-  tr_wheel3->setMatrix( osg::Matrix::translate(.350, .0, -0.350) );
-  tr_wheel3->addChild( wheel.get() );
-
-  osg::ref_ptr<osg::MatrixTransform> tr_wheel4 = new osg::MatrixTransform;
-  tr_wheel4->setMatrix( osg::Matrix::translate(-.350, .0, -0.350) );
-  tr_wheel4->addChild( wheel.get() );
+  robot_tr_body_->addChild( rotate_body.get() );
 
   /// Add the two transformation nodes to the root node and start the viewer:
   osg_root_ = new osg::Group;
   osg_root_->addChild( robot_tr_body_.get() );
-  robot_tr_body_->addChild( tr_wheel1.get() );
-  robot_tr_body_->addChild( tr_wheel2.get() );
-  robot_tr_body_->addChild( tr_wheel3.get() );
-  robot_tr_body_->addChild( tr_wheel4.get() );
 }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -127,11 +144,38 @@ void Visualiser::onOdometry(const hive::uuid& src, const std::exception_ptr& ex,
     return;
   }
 
-  osg::Vec3d pos{
+  const osg::Vec3d pos{
     odo.uncertain_pose.pose.position.x(),
         odo.uncertain_pose.pose.position.y(),
         odo.uncertain_pose.pose.position.z()};
+
+  const osg::Quat rot{
+    odo.uncertain_pose.pose.orientation.x(),
+        odo.uncertain_pose.pose.orientation.y(),
+        odo.uncertain_pose.pose.orientation.z(),
+        odo.uncertain_pose.pose.orientation.w()};
+
   robot_tr_body_->setPosition(pos);
+  robot_tr_body_->setAttitude(rot);
+
+  static Eigen::Vector4d wheels{0,0,0,0};
+  static auto last_time = hive::timing::secondsSinceEpoch();
+  const auto now = hive::timing::secondsSinceEpoch();
+  const auto dt = now - last_time;
+  last_time = now;
+  const auto wheel_speeds = motion_model_.computeWheelDelta(odo.uncertain_twist.twist.linear.x(),
+                                                            odo.uncertain_twist.twist.linear.y(),
+                                                            odo.uncertain_twist.twist.angular.z());
+  /// \todo
+  /// - set wheel positions correctly
+  /// - set wheel orientation so that rollers point in the right direction
+  /// - set up animation to rotate the wheels at speed above
+
+  wheels += wheel_speeds * dt;
+  tr_wheel1_->setAttitude(osg::Quat(wheels[0], osg::Vec3d(0,0,1)));
+  tr_wheel2_->setAttitude(osg::Quat(wheels[1], osg::Vec3d(0,0,1)));
+  tr_wheel3_->setAttitude(osg::Quat(wheels[2], osg::Vec3d(0,0,-1)));
+  tr_wheel4_->setAttitude(osg::Quat(wheels[3], osg::Vec3d(0,0,-1)));
 
 }
 
@@ -147,9 +191,8 @@ int main(int argc, const char* argv[])
 
     osgViewer::Viewer viewer;
     viewer.setUpViewInWindow(0,0,640,480);
-    osg::ref_ptr<osg::Node> scene = vis.getScene();
-    viewer.setSceneData( scene );
-    return viewer.run();
+    viewer.setSceneData( vis.getScene() );
+    viewer.setCameraManipulator( new osgGA::TrackballManipulator );
 
     while (!viewer.done() && vis.ok())
     {
