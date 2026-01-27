bd78eb9689b00bfb41d45c5e364e8571a51e5f6b
Vilas Chitrakaran <cvilas@gmail.com>
2026-01-27 08:00:56 +0000

    Fixes linter config

--- sdl_gpu_hello/cmake/linter.cmake
+++ sdl_gpu_hello/cmake/linter.cmake
@@ -3,9 +3,9 @@ if(LINTER_BIN)
   set(LINTER_ARGS
     -extra-arg=-Wno-ignored-optimization-argument
     -extra-arg=-Wno-unknown-warning-option)
-  set(LINTER_INVOKE_COMMAND ${GBS_TEMPLATES_DIR}/clang-tidy-cache.py ${LINTER_BIN} -p ${CMAKE_BINARY_DIR} ${LINTER_ARGS})
-  set(CMAKE_C_CLANG_TIDY ${LINTER_BIN})
-  set(CMAKE_CXX_CLANG_TIDY ${LINTER_BIN})
+  set(LINTER_INVOKE_COMMAND ${LINTER_BIN} -p ${CMAKE_BINARY_DIR} ${LINTER_ARGS})
+  set(CMAKE_C_CLANG_TIDY ${LINTER_INVOKE_COMMAND})
+  set(CMAKE_CXX_CLANG_TIDY ${LINTER_INVOKE_COMMAND})
 else()
   message(WARNING "Linter (clang-tidy) not found.")
 endif()
